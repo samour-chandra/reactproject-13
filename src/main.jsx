880f846e1c4ff222fa0f6863a2adb75aa42d3a2c
Samour chandra paul <scpsamour@gmail.com>
2026-02-12 00:56:17 +0600

    Add StarRating component and use in main
    Introduce a new StarRating component (src/components/StarRating.jsx) with a default maxRating prop, local rating state, inline styles, and a Star subcomponent that renders an SVG. Update src/main.jsx to import StarRating, comment out the App render and index.css import, and render two StarRating instances (maxRating=10 and maxRating=25). Note: stars are currently static (rating click handlers not wired) and there are example SVG variants left as comments for future refinement.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+import App from "./App.jsx";
+import StarRating from "./components/StarRating.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10}/>
+    <StarRating maxRating={25}/>
   </StrictMode>,
-)
+);
